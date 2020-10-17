29386b043c9b356420a4e4eff0af8bce565db49f
GermanVq <gavegaq@gmail.com>
2020-10-17 11:33:54 -0500

    Clase Automovil creada

---
+++ app/src/main/java/com/example/lista_personalizada/Automovil.java
@@ -0,0 +1,68 @@
+package com.example.lista_personalizada;
+
+public class Automovil {
+    private int foto;
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String color;
+    private Double precio;
+
+    public Automovil(int foto, String placa, String marca, String modelo, String color, Double precio) {
+        this.foto = foto;
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.precio = precio;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+}
