1f332ac0c56ae23d1d4b67e3bcd01b4b4604e1bc
GermanVq <gavegaq@gmail.com>
2020-10-17 11:26:18 -0500

    project start

---
+++ app/src/main/java/com/example/lista_personalizada/listado_autos_personalizado.java
@@ -0,0 +1,14 @@
+package com.example.lista_personalizada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listado_autos_personalizado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_autos_personalizado);
+    }
+}
